b54d5ce214c4b82b7e8c5f145c6fde602bc510c3
Cleiton Michael Lima <cleiton.lima@fundatecserver.com.br>
2018-05-24 15:12:42 -0300

    FrontEnd: Inclusão do NgRoute, Funcionando para a pagina principal do portal

--- app/app.js
+++ app/app.js
@@ -1,1 +1,17 @@
-var hospitaliza = angular.module('Hospitaliza', []);
+var hospitaliza = angular.module('Hospitaliza', ['ngRoute']);
+
+hospitaliza.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "../main.php"
+    })
+    .when("/avaliacao", {
+        templateUrl : "../avaliacao.php"
+    })
+    .when("/instituicao", {
+        templateUrl : "../instituicao.php"
+    })
+    .when("/search/:termo", {
+        templateUrl : "../search.php"
+    });
+});
---
+++ main.php
@@ -0,0 +1,108 @@
+<div  class='row center'>
+		<nav class="navbar navbar-light bg-light justify-content-between  col col-sm-12 navBarPrincipal">
+			<a class="navbar-brand">
+				<img src="img/logo_hospitaliza.png" width="15%" style='float:left;'>
+			</a>
+			<span class="form-inline">
+				<button class="btn btn-outline-secondary" type="button" id="btnCadastreseInicial" data-toggle='modal' data-target="#modalCadastre">Cadastre-se</button>	
+				<button class="btn btn-outline-success my-2 my-sm-0" data-toggle='modal' data-target="#modalLogin">Entrar</button>
+			</span>
+		</nav>
+		<div class="jumbotron col col-sm-12 jumbPrincipal" >
+			<h3 class="display-4 textJumbPrincipal">Digite o nome do hospital</h3>
+			<div class='row'>
+				<div class='col col-sm-3'>
+				</div>
+				<div class='col col-sm-5'>
+					<input id="barraProcurarInicial" type="text" class='form-control'>
+				</div>
+				<div class='col col-sm-2'>
+					<button class='btn btn-primary btn-lg' id="btnProcurarInicial"> Procurar </button>
+				</div>
+				<div class='col col-sm-2'>
+				</div>
+			</div>
+		</div>
+		<div class="row col-sm-12">
+            <div class='col-sm-12'>
+                <center>
+                    <h3>
+                        Ranking de Instituições Cadastradas no Portal
+                    </h3>
+                </center>
+            </div>
+            <div class="col-sm-1"></div>
+            <div class=" row col-sm-10">
+                <div class="col-sm-1"></div>               
+                <div class="col-sm-2 cardRanking center" >
+                    <h4> Instituições bem avaliados</h4>
+                    <ul>
+                        <li> 
+                            <b> - Instiuicao 1 </b>
+                            <br>
+                            <span class="fa fa-star checkedStar"></span>
+                            <span class="fa fa-star checkedStar"></span>
+                            <span class="fa fa-star checkedStar"></span>
+                            <span class="fa fa-star checkedStar"></span>
+                            <span class="fa fa-star"></span>
+                        </li>
+                        <li>
+                            <b> - Instiuicao 2 </b>
+                            <br>
+                            <span class="fa fa-star checkedStar"></span>
+                            <span class="fa fa-star checkedStar"></span>
+                            <span class="fa fa-star checkedStar"></span>
+                            <span class="fa fa-star "></span>
+                            <span class="fa fa-star"></span>
+                        </li>
+                        <li>
+                            <b> - Instiuicao 3 </b>
+                            <br>
+                            <span class="fa fa-star checkedStar"></span>
+                            <span class="fa fa-star checkedStar"></span>
+                            <span class="fa fa-star checkedStar"></span>
+                            <span class="fa fa-star checkedStar"></span>
+                            <span class="fa fa-star"></span>
+                        </li>
+                        <li> <b> - Instiuicao 4 </b></li>
+                        <li> <b> - Instiuicao 5 </b></li>
+                    </ul>
+                    <span>
+                        <button class='btn btn-info btn-sm'> Ver Mais </button>
+                    </span>
+                </div>  
+                <div class="col-sm-2 cardRanking center">
+                     <h4> Instituições mal avaliados</h4>
+                     <ul>
+                        <li> <b> - Instiuicao 1 </b></li>
+                        <li> <b> - Instiuicao 2 </b></li>
+                        <li> <b> - Instiuicao 3 </b></li>
+                        <li> <b> - Instiuicao 4 </b></li>
+                        <li> <b> - Instiuicao 5 </b></li>
+                    </ul>
+                </div>              
+                <div class="col-sm-2 cardRanking center">
+                     <h4> Instituições mais procuradas</h4>
+                     <ul>
+                        <li> <b> - Instiuicao 1 </b></li>
+                        <li> <b> - Instiuicao 2 </b></li>
+                        <li> <b> - Instiuicao 3 </b></li>
+                        <li> <b> - Instiuicao 4 </b></li>
+                        <li> <b> - Instiuicao 5 </b></li>
+                    </ul>
+                </div>  
+                <div class="col-sm-2 cardRanking center">
+                     <h4> Termos mais utilizados</h4>
+                     <ul>
+                        <li> <b> - Hospital e Saúde </b></li>
+                        <li> <b> - Hospital Porto Alegre </b></li>
+                        <li> <b> - dfdsfs </b></li>
+                        <li> <b> - Lorem Ipsum  </b></li>
+                        <li> <b> - tatatatataatat </b></li>
+                    </ul>
+                </div>  
+                <div class="col-sm-1" ></div>
+            </div>            
+            <div class="col-sm-1" ></div>		    
+		</div>
+	</div>
